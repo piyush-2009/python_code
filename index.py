fbbeca4cd3a0d187e0a9180428d3806c12a699ac
piyush-2009 <rathodpiyush21001@gmail.com>
2025-07-09 10:26:30 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+print("wellcome to the interactive personel date collected!")
+x= str  (input("please enter yuor name :"))
+
+a= int(input("please enter your age :"))
+
+n= float (input("please enter your hidht in meters :"))
+
+t=int (input("please enter your favourite number :"))
+ 
+print("Thank you ! Here is the infourmetion we collected :")
+
+
+print("name: Alice (type: <class 'str',> memory addrese :)")
+
+print("age : 25 (type: <class 'int' >, memory addrese :)")
+  
+print("height :5.00(type: <class 'float' >, memory addrese :")
+
+print("fevourite number :7 (type: <class 'float' >, memory addrese :")
+
+print("Your birth year is approxmately : 1998 bsed on your age of 25:" )
+
+
+print ("Thank you for using the personeal data collecter.Goodbye")
